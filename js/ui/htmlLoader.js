dd3f7c37a2df312f55aff4001aa0b328e6480b14
FURSSIO <faridfurqannumero25@gmail.com>
2026-04-21 16:08:55 +0200

    commit, divisione file aggiunta background

---
+++ js/ui/htmlLoader.js
@@ -0,0 +1,28 @@
+const HTML_PARTIALS = [
+  'html/screen-menu.html',
+  'html/screen-game.html',
+  'html/screen-over.html',
+  'html/screen-ability.html',
+  'html/screen-map-select.html',
+  'html/screen-map-complete.html',
+];
+
+async function loadHTMLPartials() {
+  const container = document.getElementById('G');
+  for (const path of HTML_PARTIALS) {
+    try {
+      const res  = await fetch(path);
+      const html = await res.text();
+      container.insertAdjacentHTML('beforeend', html);
+    } catch(e) {
+      console.error('HTMLLoader: impossibile caricare', path, e);
+    }
+  }
+}
+
+loadHTMLPartials().then(() => {
+  const s   = document.createElement('script');
+  s.src     = 'js/modes/infinite/main.js';
+  s.onerror = e => console.error('Impossibile caricare main.js', e);
+  document.head.appendChild(s);
+});
