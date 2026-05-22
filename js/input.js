08f9c2e5a731c8515a7f0180dc71ef08ac690aa2
FURSSIO <faridfurqannumero25@gmail.com>
2026-05-22 17:37:20 +0200

    commit

--- js/input.js
+++ js/input.js
@@ -308,7 +308,7 @@ document.addEventListener('keydown', e => {
 });
 
 /* ── DEV CHEATS ── */
-const DEV_CHEATS = true;
+const DEV_CHEATS = false;
 
 document.addEventListener('keydown', e => {
   if (!DEV_CHEATS || !running) return;
